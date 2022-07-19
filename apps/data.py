eb64da1e355798ba4281bf12c2f8c1a16c1fedd8
Cyril Joly <84456160+cyrjol@users.noreply.github.com>
2022-07-19 16:20:07 +0200

    Add files via upload

---
+++ apps/data.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import streamlit as st
+import pandas as pd
+from PIL import Image
+def app():
+    st.header('Data processing')
+
+    st.subheader("Correlation Matrix")
+    st.write("""
+             
+             We could use all the dataset, yet some of the variables seems to be very similar, so we could consider to avoid considering multiple features.
+             
+             We plot the correlation matrix of the features to see which variables have the most similar evolution.
+             
+             
+             """)
+    st.caption('Correlation Matrix')
+    st.dataframe(pd.read_csv('C:/Users/cjoly/projet/Net1_CMH/Scenario-1/corr.csv'))
+    
+   
+             
+    st.subheader("Final dataset") 
+    st.write("""
+             
+             At this point we can reduce the set of features to only 4 features without loosing most of the information in the original dataset.
+             
+             Moreover we are now indexing the dataframe by timestamp, and we have **scaled** the data so it would be easier to train our model in the future. 
+             
+             """)
+    st.dataframe(pd.read_csv('C:/Users/cjoly/projet/Net1_CMH/Scenario-1/df_min.csv',parse_dates=['timestamp'], index_col='timestamp'))
+    
+    st.header('Training')
+    st.write("""
+             
+             Eventhough the dataset contains labels, we want to use an **unsupervised** model which would be more accurate of a real use case where we do not have the labels instateniously.
+            
+             After considering different model, we choose to use a **PCA unsupervised model**, which is particularly efficient in case of anomaly detection.
+             
+             We train our model on the 505th scenario (abrupt with 15% of leak), so the model could detect a certain amount of anomalies.
+             
+             Here are the results :
+             """)
+             
+             
+    pred = pd.read_csv('C:/Users/cjoly/projet/Net1_CMH/Scenario-505/pred_data.csv', parse_dates=['timestamp'], index_col='timestamp')   
+    y = pd.read_csv('C:/Users/cjoly/projet/Net1_CMH/Scenario-505/y.csv', parse_dates=['timestamp'], index_col='timestamp')      
+    
+    st.caption('True Anomalies')
+    st.line_chart(y['Label'])
+    st.caption('Predicted Anomalies')
+    st.line_chart(pred['0'])
+    
+    mat =Image.open("C:/Users/cjoly/projet/Net1_CMH/Scenario-505/matrix.PNG")
+    st.image(mat, caption='Training Confusion Matrix')
+    
+    st.write("""
+             
+             Our model is not the best, yet, it has predicted most of the anomalies at the beginning, which is a good beginning.
+             
+             Let's test this model on other scenario to see how it is doing.
+             
+             """)
+    
+    st.header('Test')
+    st.write("""
+             
+             We test the model on the 2nd scenario (incipient with 5% of leak) :
+             
+             """)
+             
+    pred_2 = pd.read_csv('C:/Users/cjoly/projet/Net1_CMH/Scenario-2/pred.csv', parse_dates=['timestamp'], index_col='timestamp')   
+    y_2 = pd.read_csv('C:/Users/cjoly/projet/Net1_CMH/Scenario-2/y.csv', parse_dates=['timestamp'], index_col='timestamp')      
+    st.caption('True Anomalies') 
+    st.line_chart(y_2['Label'])
+    st.caption('Predicted Anomalies')
+    st.line_chart(pred_2['0'])
+     
+    mat =Image.open("C:/Users/cjoly/projet/Net1_CMH/Scenario-2/matrix1.PNG")
+    st.image(mat, caption='Test Confusion Matrix')        
+    
+    st.write("""
+             
+             We can see that our model has a pretty good accurracy but it is far from perfect, indeed there is a lot of noise.
+             
+             To avoid that we will try to **smooth** the data. 
+             
+             """)
+    st.subheader('Smoothing')         
+    st.write("""
+             
+                            
+            For each prediction : *we round the mean of the prediction on the last day*
+                 
+            Let's look at that!
+                
+             
+             """)
+             
+    data_2 = pd.read_csv('C:/Users/cjoly/projet/Net1_CMH/Scenario-2/pred_data.csv', parse_dates=['timestamp'], index_col='timestamp')   
+    st.caption('True Anomalies')
+    st.line_chart(y_2['Label'])
+    st.caption('Predicted Anomalies')
+    st.line_chart(data_2['0'])
+     
+    mat =Image.open("C:/Users/cjoly/projet/Net1_CMH/Scenario-2/matrix2.PNG")
+    st.image(mat, caption='Test Confusion Matrix') 
+    
+    
+    
+    st.write("""
+             
+             We have a satisfying accuraccy.
+             
+             Our model looks ready, we will apply it to 3 scenario of each type in the **PREDICTION** page.
+                
+             
+             """)
+
+
+
+
+
+
---
+++ apps/home.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 18 10:50:41 2022
+
+@author: cjoly
+"""
+import streamlit as st
+import pandas as pd
+from PIL import Image
+def app():
+    st.header(" Benchmark description")       
+    st.write("""
+     Currently, there is not a widely accessible dataset of real or realistic leakage scenarios, which could be used as a common benchmark. 
+     
+     Such a dataset should contain multiple scenarios and networks under varying conditions, to provide an objective assessment of the fault diagnosis algoritm.
+          
+     Thus, we need to work on a simulated benchmark which is comprised of a large number of realistic leakage scenarios which occur randomly at different water distribution benchmark networks, of different size and topology. 
+     
+     For each benchmark network  and  for  each  leakage  scenario *(1000 in total)*,  the  leakage  parameters  (e.g.,  number  of  leaks,  locations,size), the structural parameters (e.g., length, pipe roughness) and realistic consumer pressure-drivendemands are varied. 
+     
+     The dataset is comprised of all leakage scenario parameters, hydraulic dynamics(flows, pressures), node demands and the network model.
+     
+     Each leakage is also assigned a time profile, categorizing them as abrupt leakages, or incipient leakages which increase gradually.
+     
+     The simulated network model looks like the following picture :
+                  """)
+    network =Image.open("C:/Users/cjoly/projet/code/network.PNG")
+    st.image(network, caption='Water Distribution Network')
+    
+    st.header( "Dataset description") 
+    
+    st.write("""
+             
+             Our dataset is composed of **17520 rows** which corresponds to 1 measure every 30min during a year, and **35 features** distribute in 3 categories :
+            - **Nodes X :** values of the demand of water at each node 
+                     
+            - **Links X :** values of flows between the nodes 
+                     
+            - **Pressure X :** values of pressure on different nodes
+             
+             
+             """)   
+    st.subheader('Data cleaning')
+    st.write("""
+             First, we visualized all the different features and we noticed that :
+             
+                         
+             """)
+    
+    
+    st.caption('Demand Node 10')
+    st.line_chart(pd.read_csv('C:/Users/cjoly/projet/Net1_CMH/Scenario-1/Demands/Node_10.csv')['Value'])
+     
+    st.caption('Pressure Node 9')
+    st.line_chart(pd.read_csv('C:/Users/cjoly/projet/Net1_CMH/Scenario-1/Pressures/Node_9.csv')['Value'])
+    
+    st.write("""
+             Those 2 features **(Demand Node 10 & Pressure Node 9)** are null for every scenario, thus we will not consider them in our dataset.
+             
+             """)
+    
+     
+    st.subheader("Full dataset")   
+    st.write("""
+             
+             As this point, our data is ready to be manipulated, as we will see on the **DATA** section
+             
+             """)
+    st.dataframe(pd.read_csv('C:/Users/cjoly/projet/Net1_CMH/Scenario-1/df_full.csv'))
+    
+    
+    
