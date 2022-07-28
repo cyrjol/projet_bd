6db53795abb9e0885ffa33bb6a5c1c27550e6b1f
Cyril Joly <84456160+cyrjol@users.noreply.github.com>
2022-07-28 15:06:21 +0200

    Update data.py

--- apps/data.py
+++ apps/data.py
@@ -11,16 +11,8 @@ import pandas as pd
 from PIL import Image
 def app():
     st.header('Data processing')
-    
-    st.write("""
-             
-             After some experimentation, we choose to do the preprocessing and the training on the 505th scenario because it is an abrupt scenario with a total of 20% of leak on the year,
-             so the model has a certain amount of leak to detect. 
-             
-             
-             """)
-
-    st.subheader("Correlation Matrix")
+      
+   
     st.write("""
              
              We could use all the dataset, yet some of the variables seems to be very similar, so we could consider to avoid considering multiple features.
@@ -29,18 +21,48 @@ def app():
              
              
              """)
-    st.caption('Correlation Matrix')
-    st.dataframe(pd.read_csv('data/scenario1/corr.csv'))
+    st.caption('Correlation Matrix Nodes')
+    corr_1=pd.read_csv('data/scenario1/corr_1.csv')
+    st.dataframe(corr_1.style.background_gradient())
     
-   
+    st.write("""
              
+             As we can see, all the Nodes are higlhy correlated, thus we can keep only **Node 11**.
+             
+             """)
+    
+    st.caption('Correlation Matrix Links')
+    corr_2=pd.read_csv('data/scenario1/corr_2.csv')
+    st.dataframe(corr_2.style.background_gradient())
+    
+    st.write("""
+             
+             As we can see, only 3 links have not over 0.9 correlation with **Node 11** :
+                 
+            - *Link 21*
+                 
+            - *Link 22*
+                 
+            - *Link 110*
+             
+            Yet, *Link 110* has 0.70 correlation with *Link 21* and considering we have already two Links features, we can also eliminate *Link 110*.
+             """)
+    
+    st.caption('Correlation Matrix Pressures')
+    corr_3=pd.read_csv('data/scenario1/corr_3.csv')
+    st.dataframe(corr_3.style.background_gradient())
+    
+    st.write("""
+            
+             As we can see, all the Pressures have 1.00 correlation between each other, thus we can keep only **Pressure 10**.
+            
+            """)
+    
     st.subheader("Final dataset") 
     st.write("""
-             We wanted at least 1 feature for each categorie ( Demands, Links, Pressures) and we eliminate the others features with more than **0.9** correlation.
+             We wanted at least 1 feature for each categorie ( Demands, Links, Pressures) and we have reduce the set of features to only **4 features** without loosing most of the information in the original dataset.
              
-             Thus we can reduce the set of features to only **4 features** without loosing most of the information in the original dataset.
-             
-             Moreover we are now indexing the dataframe by timestamp, and we have **scaled** the data so it would be easier to train our model in the future. 
+             Moreover we have **scaled** the data so it would be easier to train our model in the future. 
              
              """)
     st.dataframe(pd.read_csv('data/scenario1/df_min.csv',parse_dates=['timestamp'], index_col='timestamp'))
@@ -49,15 +71,26 @@ def app():
     st.write("""
              
              We could have used 2 approach:
+                 
                 - Time Series
+                
                 - Unsupervised Classification 
             
-            We choose the 2nd option because it had better results.             
+             We choose the 2nd option because it was the easiest to begin with and finally it had better results. 
+            
              Eventhough the dataset contains labels, we want to use an **unsupervised** model which would be more accurate of a real use case where we do not have the labels instateniously.
             
-             We consider different unsupervised model especially effective on anomaly detection case like *kNN, Isolation Forest, (Variational) Autoencoder* and *GAN*.
+             We used the *PYOD* API which includes different algorithms dedicated to anomaly detection. Thus, we tried some unsupervised model like *kNN, Isolation Forest and Autoencoder*.
              
              At the end, we had the best results with a **PCA** model.
+             
+             Principal component analysis (PCA) can be used in detecting outliers. PCA is a linear dimensionality reduction using Singular Value Decomposition of the data to project it to a lower dimensional space.
+
+            In this procedure, covariance matrix of the data can be decomposed to orthogonal vectors, called eigenvectors, associated with eigenvalues. The eigenvectors with high eigenvalues capture most of the variance in the data.
+
+            Therefore, a low dimensional hyperplane constructed by k eigenvectors can capture most of the variance in the data. However, outliers are different from normal data points, which is more obvious on the hyperplane constructed by the eigenvectors with small eigenvalues.
+
+            Therefore, outlier scores can be obtained as the sum of the projected distance of a sample on all eigenvectors.
              
                           
              Here are the results :
@@ -66,64 +99,97 @@ def app():
              
     pred = pd.read_csv('data/scenario505/pred_data.csv', parse_dates=['timestamp'], index_col='timestamp')   
     y = pd.read_csv('data/scenario505/y.csv', parse_dates=['timestamp'], index_col='timestamp')      
+   
     y = y.merge(pred,how='left', left_index=True, right_index=True)
+    
     y.rename(columns={'Label': 'True', '0':'Predicted'},inplace=True)
+    
     st.caption('Anomalies')
-    st.line_chart(y[['True','Predicted']])
+    plot=px.scatter(x=y.index,y='Predicted', data_frame=y,color="True")
+    st.plotly_chart(plot)
+    
+    st.write("""
+             The color of the points corresponds to the true value of leakage :
+            
+            - Blue : there is no leak
+            
+            - Yellow : there is a leak
+            
+            The position of the points corresponds to the predicted value of leakage :
+            
+            - Top : predicts a leak
+            
+            - Bottom : predicts no leak
+             
+             
+             
+             """)
+    
     
     mat =Image.open("data/scenario505/matrix.png")
     st.image(mat, caption='Training Confusion Matrix')
+    st.caption("""
+               
+               Line: predicted value 
+               
+               Column : real value
+               
+               Class A : No Leak
+               
+               Class B : Leak 
+               
+               
+               """)
     
-    st.write("""           
-             Our model is not perfect, yet we witness two interesting points to begin :
-                - Our model has a few false anomalies ( 2% of the predicted anomalies )
-                - The beginning of the leak is well predicted
-             
-             Let's test this model on other scenario to see how it is doing.
-             
-             """)
     
     st.header('Test')
     st.write("""
              
-             We test the model on the 2nd scenario : incipient with 5% of leak over the year, to see how the model is doing on a more subtle leak.
+              We test the model on the 2nd scenario : incipient with 5% of leak over the year, to see how the model is doing on a more subtle leak.
              
              """)
              
     pred_2 = pd.read_csv('data/scenario2/pred.csv', parse_dates=['timestamp'], index_col='timestamp')   
     y_2 = pd.read_csv('data/scenario2/y.csv', parse_dates=['timestamp'], index_col='timestamp')      
+    
     y = y_2.merge(pred_2,how='left', left_index=True, right_index=True)
+    
     y.rename(columns={'Label': 'True', '0':'Predicted'},inplace=True)
+    
     st.caption('Anomalies')
-    st.line_chart(y[['True','Predicted']])
+    plot=px.scatter(x=y.index,y='Predicted', data_frame=y,color="True")
+    st.plotly_chart(plot)
      
     mat =Image.open("data/scenario2/matrix1.png")
     st.image(mat, caption='Test Confusion Matrix')        
     
     st.write("""
              
-             We can see that our model has a pretty good metrics :
-                - Negative Predictive Value : 84,11%
-                - Specify Rate : 68,77 %
-             
-             Yet, it is far from perfect because there is a lot of noise.
-             
-             To avoid that we will try to **smooth** the data. 
+             We can see that our model has pretty good metrics :
+                - Negative Predictive Value : **84,11%** , much better than before, the leak is predicted very soon (4h). 
+                - Specify Rate : **68,77 %**, there is a lot of noise ( blue points on top), but if we zoom in, we can see that these points are isolated, to avoid that we will try to **smooth** the data. 
+              
              
              """)
     st.subheader('Smoothing')         
     st.write("""                                       
             We have noticed that most false anomalies are isolated (1 or 2 per day), but if there is a leak it lasts several days.
-            Thus, our smoothing method is to consider that there is an anomaly if the average over the last 24 hours >50%.     
+            
+            Thus, we have consider different smoothing method : average over the last X hours > Y%.
+            
+            We have kept the method which consider that there is an anomaly if the average over the last 24 hours >50%, because it had good results and this is consistent, it means that we can detect the leak at least 12 hours after it starts which is acceptable.     
             
             Let's look at that!
                                      """)
              
     data_2 = pd.read_csv('data/scenario2/pred_data.csv', parse_dates=['timestamp'], index_col='timestamp')   
     y = y_2.merge(data_2,how='left', left_index=True, right_index=True)
+    
     y.rename(columns={'Label': 'True', '0':'Predicted'},inplace=True)
+    
     st.caption('Anomalies')
-    st.line_chart(y[['True','Predicted']])
+    plot=px.scatter(x=y.index,y='Predicted', data_frame=y,color="True")
+    st.plotly_chart(plot)
      
     mat =Image.open("data/scenario2/matrix2.png")
     st.image(mat, caption='Test Confusion Matrix') 
@@ -133,13 +199,14 @@ def app():
     st.write("""
              
              Now the metrics are impressive :
-                - Negative Predictive Value : 84%
-                - Specify Rate : 100 %
+                - Negative Predictive Value : **84%**, we have lost only 0.11%, now we predict the leak after *15h*. 
+                - Specify Rate : **100 %**, we have eliminated all the noise.
                 
-             We have eliminated all the false anomalies and with our smoothing filter we can detect a leak 12h after its beginning.
+                         
+             Therefore, we lost a little speed of detection in favor of cleaning the results.
              
-             Our model looks ready, we will apply it to 3 scenario of each type in the **PREDICTION** page.
-                
+             Yet, when we apply this filter to more scenario, this is also very efficient as we will see on the **PREDICTION** page.
+               
              
              """)
 
