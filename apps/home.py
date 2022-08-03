88f69a8c9ec06ee60fbdf7854541ebad53ccece5
Cyril Joly <84456160+cyrjol@users.noreply.github.com>
2022-08-03 11:30:27 +0200

    updates

--- apps/home.py
+++ apps/home.py
@@ -11,13 +11,21 @@ def app():
     st.header(" Benchmark description")       
     st.write("""
     Currently, there is not a widely accessible dataset of real or realistic leakage scenarios, which could be used as a common benchmark, such a dataset should contain multiple scenarios under varying conditions.
+    
     Thus, we need to work on a simulated benchmark which consists of a large number of realistic leakage scenarios which occur randomly, of different size and intensity.
+    
     For each leakage scenario (1000 in total), the leakage parameters (e.g., number of leaks, locations,size), the structural parameters (e.g., length, pipe roughness) and realistic consumer pressure-driven demands are varied.
+    
     The dataset comprises all leakage scenario parameters, hydraulic dynamics(flows, pressures), node demands and the network model.
+    
     Each leakage is also assigned a time profile :
+    
     -**No leak**
+    
     -**Abrupt** : the leak is of constant intensity
+    
     -**Incipient** : the leak increases gradually which makes it more difficult to detect.
+    
     Moreover, each leakage may remain for a longer or shorter period of time depending on whether the leak is found and repaired, corresponding to a % of leak. Thus we will present the different scenario as : scenario X with Y% of leak
 
                   """)
