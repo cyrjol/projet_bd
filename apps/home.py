8dda384a23324eeeac9d498ecc6dae634a843666
Cyril Joly <84456160+cyrjol@users.noreply.github.com>
2022-08-04 10:15:43 +0200

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -10,25 +10,19 @@ from PIL import Image
 def app():
     st.header(" Benchmark description")       
     st.write("""
-    Currently, there is not a widely accessible dataset of real or realistic leakage scenarios, which could be used as a common benchmark, such a dataset should contain multiple scenarios under varying conditions.
+    Currently, there is not a widely accessible dataset of real or realistic leakage scenarios.
     
-    Thus, we need to work on a simulated benchmark which consists of a large number of realistic leakage scenarios which occur randomly, of different size and intensity.
-    
-    For each leakage scenario (1000 in total), the leakage parameters (e.g., number of leaks, locations,size), the structural parameters (e.g., length, pipe roughness) and realistic consumer pressure-driven demands are varied.
-    
-    The dataset comprises all leakage scenario parameters, hydraulic dynamics(flows, pressures), node demands and the network model.
-    
-    Each leakage is also assigned a time profile :
-    
+    Thus, we need to work on a simulated benchmark which consists of a large number of realistic leakage scenarios **(1000)** which occur randomly, with one of the 3 different types of scenarios : 
+          
     -**No leak**
     
     -**Abrupt** : the leak is of constant intensity
     
     -**Incipient** : the leak increases gradually which makes it more difficult to detect.
-    
+           
     Moreover, each leakage may remain for a longer or shorter period of time depending on whether the leak is found and repaired, corresponding to a % of leak. Thus we will present the different scenario as : scenario X with Y% of leak
-
-                  """)
+    
+                      """)
     network =Image.open("network.PNG")
     st.image(network, caption='Water Distribution Network')
     
@@ -50,7 +44,7 @@ def app():
     st.subheader("Full dataset")   
     st.write("""
              
-             As this point, our data is ready to be manipulated, here is an example of the raw data of the 1st scenario 
+             As this point, our data is ready to be manipulated, here is an example of the raw data of the 1st scenario. 
              
              """)
     st.dataframe(pd.read_csv('data/scenario1/df_full.csv'))
